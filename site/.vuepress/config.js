ae87a8760639c9f96b6adcc237679892544bd94b
John Ouellet <john@thinktandem.io>
2020-03-06 10:56:01 -0500

    Tweak site vuepress

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -1,20 +1,9 @@
-const webpack = require('webpack');
-
 module.exports = {
-  title: 'Landob.log',
-  description: 'A liberating blog by and for professional developers. We share things about tech, DevOps and development using but not limited to Lando.',
-  configureWebpack: config => {
-    return {plugins: [
-      new webpack.EnvironmentPlugin({
-        LANDO_API: process.env.LANDO_API || 'https://api.lando.dev',
-      }),
-    ]};
-  },
+  title: 'Tandem',
+  description: 'Tandem uses open-source tools like Drupal, Laravel, and Docker to transform your complex workflows and technical debt into elegant web applications.',
   head: [
-    ['link', {rel: 'icon', href: '/favicon.ico'}],
+    ['link', {rel: 'icon', href: '/favicon.png'}],
     ['link', {rel: 'stylesheet', href: '/styles/overrides.css'}],
-    ['link', {rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Poppins:700|Source+Sans+Pro&display=swap'}],
-    ['script', {src: '//js.hs-scripts.com/6478338.js'}],
   ],
   plugins: {
     '@vuepress/blog': {
@@ -27,9 +16,9 @@ module.exports = {
         },
       ],
       feed: {
-        canonical_base: 'https://blog.lando.dev',
+        canonical_base: 'https://thinktandem.io',
         feed_options: {
-          favicon: 'https://lando.dev/favicon.ico',
+          favicon: 'https://thinktandem.io/favicon.png',
           image: 'https://lando.dev/images/logo-pink-small.png',
         },
       },
@@ -45,52 +34,36 @@ module.exports = {
         },
       ],
       sitemap: {
-        hostname: 'https://blog.lando.dev',
+        hostname: 'https://thinktandem.io',
       },
     },
     '@vuepress/google-analytics': {
-      ga: 'UA-74237404-6',
+      ga: 'UA-XXXXXXX',
     },
     'autometa': {
       site: {
-        name: 'Lando',
-        twitter: 'devwithlando',
+        name: 'Tandem',
+        twitter: 'ThinkTandem',
       },
-      canonical_base: 'https://blog.lando.dev',
+      canonical_base: 'https://thinktandem.io',
     },
     'robots': {
-      host: 'https://blog.lando.dev',
+      host: 'https://thinktandem.io',
       sitemap: '/sitemap.xml',
     },
   },
   theme: '@vuepress/theme-blog',
   themeConfig: {
     logo: '/images/logo-pink-small.png',
-    docsDir: 'blog',
+    docsDir: 'site',
     docsBranch: 'master',
     search: false,
     editLinks: false,
-    nav: [
-      {
-        text: 'Case Studies',
-        link: '/tag/case-study/',
-      },
-        {
-        text: 'Development',
-        link: '/tag/development/',
-      },
-      {
-        text: 'DevOps',
-        link: '/tag/devops/',
-      },
-      {
-        text: 'Workflows',
-        link: '/tag/workflows/',
-      },
-      {
-        text: 'Lando',
-        link: '/tag/lando/',
-      },
-    ],
+    // nav: [
+    //   {
+    //     text: 'Case Studies',
+    //     link: '/tag/case-study/',
+    //   },
+    // ],
   },
 };
