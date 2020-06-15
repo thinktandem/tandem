881761b9cd8c67360e95231b6317a3d24e44121f
Dustin LeBlanc <dustin@dustinleblanc.com>
2020-06-15 16:47:29 -0400

    Issue #161: First draft of guide on emergencies.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -69,6 +69,7 @@ module.exports = {
             'quarterly-review',
             'security-basics',
             'security-incident-response',
+            'handling-emergency-requests'
           ],
         },
         {
