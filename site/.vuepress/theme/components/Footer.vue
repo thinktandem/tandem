c5b9f74bdc77a8390519047f910bedf3bbddb295
Thiago de Mello Bueno <thiago@madeofpeople.org>
2021-01-21 14:36:49 -0500

    Footer update

--- site/.vuepress/theme/components/Footer.vue
+++ site/.vuepress/theme/components/Footer.vue
@@ -1,49 +1,46 @@
 <template>
-  <footer id="footer">
-    <div class="footer-container">
-      <div class="footer-copyright">
-        <
-        <span class="copyright">© 2021</span> Tandem
-      </div>
-      <div class="footer-links">
-        <div
-          class="menu-primary"
+  <footer id="footer" class="site__footer">
+    <div class="copyright">
+      © 2021 Tandem
+    </div>
+    <div class="footer-links">
+      <div
+        class="menu-primary"
+      >
+        <ul
+          v-if="$themeConfig.nav"
         >
-          <ul
-            v-if="$themeConfig.nav"
+          <li
+            v-for="item in $themeConfig.nav"
+            :key="item.text"
           >
-            <li
-              v-for="item in $themeConfig.nav"
-              :key="item.text"
-            >
-              <NavLink :link="item.link">
-                {{ item.text }}
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-        <div
-          class="menu-secondary"
-        >
-          <NavLink link="/careers/">
-            Careers
-          </NavLink>
-          <NavLink link="https://handbook.thinktandem.io">
-            Handbook
-          </NavLink>
-          <NavLink link="https://twitter.com/thinktandem">
-            Twitter
-          </NavLink>
-          <NavLink link="https://github.com/thinktandem">
-            GitHub
-          </NavLink>
-          <NavLink link="/terms/">
-            Terms of Use
-          </NavLink>
-          <NavLink link="/privacy/">
-            Privacy Policy
-          </NavLink>
-        </div>
+            <NavLink :link="item.link">
+              {{ item.text }}
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div
+        class="menu-secondary"
+      >
+        <NavLink link="/careers/">
+          Careers
+        </NavLink>
+        <NavLink link="https://handbook.thinktandem.io">
+          Handbook
+        </NavLink>
+        <NavLink link="https://twitter.com/thinktandem">
+          Twitter
+        </NavLink>
+        <NavLink link="https://github.com/thinktandem">
+          GitHub
+        </NavLink>
+        <NavLink link="/terms/">
+          Terms of Use
+        </NavLink>
+        <NavLink link="/privacy/">
+          Privacy Policy
+        </NavLink>
       </div>
     </div>
   </footer>
@@ -54,31 +51,26 @@ export default {};
 </script>
 
 <style lang="stylus">
-#footer
+.site__footer
   background-color white
-.footer-container
-  padding 3em 0em
+  padding 3rem 2rem
   display flex
   max-width 1140px
   margin auto
   border-top 1px solid $borderColor
-  background-color white
-  .footer-copyright, .footer-links
+  .copyright, .footer-links
     padding 0
     margin 0
-  .footer-copyright
-    width 32%
-    text-align left
+  .copyright
+    { displayType }
     font-size typeScale.small
-    align-self center
-    .copyright
-      color $tandemPink
-      font-weight 900
+    color $tandemPink
+    width 30vw
   .footer-links
-    width 70%
-    text-align right
+    width 70vw
     a
       { displayType }
+      text-align right
       text-decoration none
 
     .menu-primary
@@ -99,56 +91,60 @@ export default {};
           margin-left 30px
     .menu-secondary
       margin-top .5em
-      margin-right -12px
-      a
+      display flex
+      justify-content flex-end
+      .nav-link
         font-size typeScale.small
         margin-left 0
         color $middleGrey
-        &:after
+        text-align right
+        position relative
+        left 0.75rem
+        &::before, &::after
+          display inline-block
+          width 0.5rem
+        &::after
           content '\00a0\00a0'
-        &:before
+        &::before
           content '\00a0\00a0'
         &:hover
           color $tandemPink
-          &:after
-            content: " ]"
-          &:before
-            content: "[ "
+          &::after
+            content " ]"
+          &::before
+            content "[ "
 @media (max-width $MQMobile)
-  .footer-container
-    flex-direction column-reverse
-    margin auto 0.5em
-    .footer-copyright, .footer-links
-      flex 1 1
-      text-align center
-      margin auto
-      padding 0 0.5em
-    .footer-copyright
-      margin-top 25px
-      width 75%
-      font-size typeScale.small
-    .footer-links
-      width 75%
-      .menu-primary
-        ul
-          justify-content center
-          margin-left -1.25rem
-          li
-            margin-left 1.25rem
-      .menu-secondary
-        margin 25px 0 0
-        a
-          font-size typeScale.small
-          line-height 2
+  flex-direction column-reverse
+  margin auto 0.5em
+  .copyright, .footer-links
+    flex 1 1
+    justify-content flex-end
+    text-align center
+    margin auto
+    padding 0 0.5em
+  .copyright
+    margin-top 25px
+    font-size typeScale.small
+  .footer-links
+    .menu-primary
+      ul
+        justify-content center
+        margin-left -1.25rem
+        li
+          margin-left 1.25rem
+    .menu-secondary
+      margin 25px 0 0 0
+      a
+        font-size typeScale.small
+        line-height 2
 @media (max-width $MQMobileNarrow)
-  .footer-container
-    .footer-links
-      .menu-primary
-        ul
-          justify-content center
-          margin-left -10px
-          li
-            margin-left 15px
-        a
-          font-size 18px
+  .footer-links
+    .menu-primary
+      ul
+        justify-content center
+        margin-left -10px
+        li
+          margin-left 15px
+      a
+        font-size 18px
 </style>
